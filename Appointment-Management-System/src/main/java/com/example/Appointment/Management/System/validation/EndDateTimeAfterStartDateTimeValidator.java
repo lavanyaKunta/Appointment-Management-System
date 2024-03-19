0a1e4761a13d3d49acd1cd95286289cb7933eee6
lavanyaKunta <lavanya.kunta@cjsstechnologies.com>
2024-03-19 18:31:29 +0530

    Appointment management System using postman with basic security

---
+++ Appointment-Management-System/src/main/java/com/example/Appointment/Management/System/validation/EndDateTimeAfterStartDateTimeValidator.java
@@ -0,0 +1,28 @@
+package com.example.Appointment.Management.System.validation;
+
+import com.example.Appointment.Management.System.model.DoctorScheduleModel;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.lang.annotation.Annotation;
+import java.time.LocalDateTime;
+
+public class EndDateTimeAfterStartDateTimeValidator implements ConstraintValidator<EndDateTimeAfterStartDateTime,DoctorScheduleModel> {
+    @Override
+    public void initialize(EndDateTimeAfterStartDateTime constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(DoctorScheduleModel dto, ConstraintValidatorContext context) {
+//        return false;
+
+        LocalDateTime startDateTime = dto.getStartDateTime();
+        LocalDateTime endDateTime = dto.getEndDateTime();
+
+        // Add your validation logic here
+//        System.out.println(startDateTime.isAfter(endDateTime));
+//        System.out.println(endDateTime.isAfter(startDateTime));
+        return endDateTime.isAfter(startDateTime);
+    }
+}
